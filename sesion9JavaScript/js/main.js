88760b958b9179b51ff35624a3d05ba6caa03e45
karenascencio <ascencio.karen@yahoo.com.mx>
2021-06-16 18:59:41 -0500

    added sesion 9 code

---
+++ sesion9JavaScript/js/main.js
@@ -0,0 +1,10 @@
+var firstNumber = prompt ("Please, type one number")
+var secondNumber = prompt ("now, write a second number")
+
+var numberOne = parseInt(firstNumber)
+var numberTwo = parseInt(secondNumber)
+
+
+
+console.log (  numberOne + numberTwo)
+console.log ( numberOne - numberTwo)
