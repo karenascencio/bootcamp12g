9b7cabb61a928226fa8498c5a748b7d9ddd3822d
karenascencio <ascencio.karen@yahoo.com.mx>
2021-06-21 16:09:44 -0500

    added 5th exercise

--- p11conditionals/js/main.js
+++ p11conditionals/js/main.js
@@ -4,7 +4,15 @@ Output: 13
 Input: el pájaro dev
 Output: 5*/
 
+function howManyVowels (string){
+    var vowelsArray = string.match(/[aeiou]/ig)
 
+    return vowelsArray.length
+}
+
+var string = prompt("Lets count how many vowels does your string have")
+
+howManyVowels(string)
 
 
 /*2.
@@ -13,6 +21,15 @@ Output: 9
 Input: Me volví loco, con largos intervalos de horrible cordura
 Output: 9*/
 
+function notALetter(string){
+
+    var onlyLetters = string.match(/[a-z]/ig)
+
+    return string.length - onlyLetters.length
+}
+
+var string = prompt("Let's count how many NOT LETTERS we have in any string, so type it")
+notALetter(string)
 
 
 /*3.
@@ -50,4 +67,67 @@ function isPalindrome (string){
 Crear una función que evalúe un string con las siguientes pruebas:
 Tener al menos 8 carácteres
 Tener al menos un número
-Tener al menos una mayúscula*/
+Tener al menos una mayúscula
+Devolver true si el string cumple con las reglas, o imprimir en 
+consola la regla o reglas que no se están cumpliendo*/
+
+
+
+
+
+
+
+/*5.
+crear una función que genere un string con "n" caracteres aleatorios,  con la única condición de que no haya caracteres repetidos en el string
+ejemplo correcto:
+input: 10
+output: asdfqwerzx
+*/
+
+/*
+-creamos una variable para guardar la cadena resultante
+-obtener el número de caracteres
+-definir un rango numérico que determine el código asccii de los caracteres que estaran disponibles
+-obtener de forma aleatoria algún número dentro de ese rango
+-obtener el caracter correspondiente al número seleccionado
+-almacenamos en una variable el caracter obtenido
+
+-evaluamos si el caracter existe en la cadena de resultado
+--si el caracter no existe, lo agregamos a la cadena de resultado
+--si el caracter existe en la cadena, debemos generar un nuevo caracter
+-repetir n veces
+-devolver la cadena de resultado
+*/
+
+//var n = parseInt( prompt("Qué longitud debe tener el string? "))
+
+/*
+function createRandomString ( charactersQty ){
+    var result = ""
+    for( i = 0; i < charactersQty; i++ ){
+        result += evaluateCharacter( result )
+    }
+    console.log('result ', result)
+    console.log('length ', result.length )
+}
+*/
+
+
+function evaluateCharacter( completeString ){
+    var characterCode = Math.floor( Math.random() * (91 - 48) + 48 )
+    var character = String.fromCharCode( characterCode )
+    return !completeString.includes(character) ? character : evaluateCharacter( completeString )
+}
+
+function createRandomString ( charactersQty ){
+    var result = ""
+    while( result.length < charactersQty ){
+        result += evaluateCharacter(result)
+    }
+    console.log('result ', result)
+    console.log('length ', result.length )
+}
+
+
+var charactersQty = prompt("Please enter how many random characters you need (up to 43)")
+createRandomString(charactersQty)
